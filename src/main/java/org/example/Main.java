42977cbbb8595d677c5e8be45941169097e4ab5a
eevgen <evgeniy.kalenichenko@gmail.com>
2023-07-27 16:26:05 +0300

    Add second counter and made that first and second Threads are going to work on incrementFirstNumber(), and third and fourth Threads are going to work on IncrementSecondNumber()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,36 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    private static int counter = 0;
+    private static int counter1 = 0;
+
+    private static int counter2 = 0;
 
     private static final int INCREMENT_ALL_NUMBERS_FIRST_THREAD = 100;
     private static final int INCREMENT_ALL_NUMBERS_SECOND_THREAD  = 300;
 
+    private static final String MESSAGE_TEMPLATE_FOR_COUNTERS = "Number = %d\n";
+
     public static void main(String[] args){
 
-        Thread thread1 = creatingNewThreads(INCREMENT_ALL_NUMBERS_FIRST_THREAD);
-        Thread thread2 = creatingNewThreads(INCREMENT_ALL_NUMBERS_FIRST_THREAD);
-        Thread thread3 = creatingNewThreads(INCREMENT_ALL_NUMBERS_SECOND_THREAD);
-        Thread thread4 = creatingNewThreads(INCREMENT_ALL_NUMBERS_SECOND_THREAD);
+        Thread thread1 = creatingNewThreads(INCREMENT_ALL_NUMBERS_FIRST_THREAD, i -> incrementFirstNumber());
+        Thread thread2 = creatingNewThreads(INCREMENT_ALL_NUMBERS_FIRST_THREAD, i -> incrementFirstNumber());
+        Thread thread3 = creatingNewThreads(INCREMENT_ALL_NUMBERS_SECOND_THREAD, i -> incrementSecondNumber());
+        Thread thread4 = creatingNewThreads(INCREMENT_ALL_NUMBERS_SECOND_THREAD, i -> incrementSecondNumber());
 
         startThreads(thread1, thread2, thread3, thread4);
 
         joinThreads(thread1, thread2, thread3, thread4);
 
-        System.out.println(counter);
-
+        printCounters(counter1, counter2);
     }
 
-    private static Thread creatingNewThreads(int amountOfIncrementing) {
-        return new Thread(() -> IntStream.range(0, amountOfIncrementing).forEach(i -> incrementNumber()));
+    private static Thread creatingNewThreads(int amountOfIncrementing, IntConsumer operation) {
+        return new Thread(() -> IntStream.range(0, amountOfIncrementing).forEach(operation));
     }
 
     private static void startThreads(Thread... threads) {
@@ -43,7 +47,14 @@ public class Main {
         }
     }
 
-    private synchronized static void incrementNumber() {
-        counter++;
+    private static void printCounters(Integer... integers) {
+        Arrays.stream(integers).forEach(integer -> System.out.printf(MESSAGE_TEMPLATE_FOR_COUNTERS, integer));
+    }
+
+    private synchronized static void incrementFirstNumber() {
+        counter1++;
+    }
+    private synchronized static void incrementSecondNumber() {
+        counter2++;
     }
 }
